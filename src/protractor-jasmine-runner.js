32e34f7f4660973fea8ef242867e9916349ca77f
Jon Hoguet <jonathan.hoguet@dealer.com>
2015-04-05 21:33:25 -0400

    now really working with jasmine 2.0

--- src/protractor-jasmine-runner.js
+++ src/protractor-jasmine-runner.js
@@ -2,12 +2,10 @@ var specs = require('jasmine-cucumber'),
     jasmineFeatureRunner = require('jasmine-cucumber/src/jasmine-feature-runner.js')(specs.features, specs.steps);
 
 describe('', function(){
-    setTimeout(function(){
-        specs.features.forEach(jasmineFeatureRunner);
-        var scenarios = specs.features.reduce(function(memo, f){
-            return memo + f.scenarios.length;
-        }, 0);
+    specs.features.forEach(jasmineFeatureRunner);
+    var scenarios = specs.features.reduce(function(memo, f){
+        return memo + f.scenarios.length;
+    }, 0);
 
-        console.log('Found ' + specs.features.length + ' features with ' + scenarios + ' scenarios');
-    });
+    console.log('Found ' + specs.features.length + ' features with ' + scenarios + ' scenarios');
 });
--- test/config.js
+++ test/config.js
@@ -2,5 +2,14 @@ var cucumber = require('../src/index.js');
 
 module.exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: cucumber.injectFiles('spec.js')
+  specs: cucumber.injectFiles('spec.js'),
+  framework: "jasmine2",
+  onPrepare: function() {
+      var SpecReporter = require('jasmine-spec-reporter');
+      // add jasmine spec reporter
+      jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
+   },
+   jasmineNodeOpts: {
+       print: function() {} // remove dot printer
+    }
 };
